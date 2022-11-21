30756e36cdd49ae31432bf26aa3cf878c3423eeb
Ramilade <rami0904@stud.kea.dk>
2022-11-21 11:16:24 +0100

    First commit

---
+++ src/test/java/com/example/bilabonnement/BilAbonnementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilAbonnementApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
