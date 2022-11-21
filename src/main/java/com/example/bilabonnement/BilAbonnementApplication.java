6690a98f99c92eeedeba5df6588e8e239984b81e
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2022-11-21 15:48:42 +0100

    Added Initial Class Diagram. Created together all of us.

--- src/main/java/com/example/bilabonnement/BilAbonnementApplication.java
+++ src/main/java/com/example/bilabonnement/BilAbonnementApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BilAbonnementApplication {
 
-  //Test Jonathan
   public static void main(String[] args) {
     SpringApplication.run(BilAbonnementApplication.class, args);
   }
