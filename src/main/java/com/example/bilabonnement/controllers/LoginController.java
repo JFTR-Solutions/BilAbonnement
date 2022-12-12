b0fe4c5385269643474468c91380fa3df2e8f66d
FHallengreen <fred70p9@stud.kea.dk>
2022-12-12 22:10:11 +0100

    minor changes to the logincontroller exception logic & added comments to all methods.

--- src/main/java/com/example/bilabonnement/controllers/LoginController.java
+++ src/main/java/com/example/bilabonnement/controllers/LoginController.java
@@ -22,6 +22,8 @@ public class LoginController {
     UserService userService;
     Encrypter e = new Encrypter();
 
+
+//    The loginController constructor is used to initialize the userService and put exception messages into the static HashMap created above.
     //FREDERIK
     public LoginController(UserService userService) {
         exceptionEnums.put(carExceptionEnum.NO_LOGIN, "Du skal være logget på for at få vist denne side");
@@ -32,6 +34,12 @@ public class LoginController {
         exceptionEnums.put(carExceptionEnum.USER_EXIST, "En bruger med denne email eksisterer allerede");
         this.userService = userService;
     }
+
+    /*validateLogin is called upon in every getMapping to validate that the user is logged in and assigned a role that grants access to the requested page.
+    * If validateUser returns false, a carLeasingException will be thrown with a message that the user needs to login.
+     * The validateRoles method is checking if the userrole contains the role attribute that has been defined in each controller or if the they have the sysadmin role.
+     * validateRoles will also throw a carLeasingException if false but will instead tell the user that they do not have permission to view the page.
+    * */
     //FREDERIK
     public Boolean validateLogin(HttpSession httpSession, String role) throws CarLeasingException {
         if (!validateUser(httpSession)) {
@@ -42,6 +50,13 @@ public class LoginController {
             return true;
         }
     }
+
+
+    /*loginPage is the first page the user will see when opening https://jftr-solution.azurewebsites.net/ . This method
+    * will catch exceptions thrown from validateuser and display the errormessage if thrown. By using Thymeleaf (th:if),
+    *  we can tell the html page to only display the error if the attribute has been created.
+    * The method is also checking if the user is already logged in, if so they will be sent directly to the welcome page instead of displaying the loginpage */
+
     //FREDERIK
     @GetMapping("/")
     public String loginPage(HttpSession httpSession, Model model) {
@@ -55,6 +70,10 @@ public class LoginController {
         return "index";
     }
 
+    /* The login postMapping method will check the input with the database. If no exception is thrown,
+    the httpsession attributes "email" & "password" will be set and user is redirected to the welcome page.
+    Else the error attribute will be set instead and the user will be sent back to the loginpage.
+   */
     //Frederik + Thomas
     @PostMapping("/login")
     public String login(@RequestParam("email") String email, @RequestParam("password") String password,
@@ -67,10 +86,13 @@ public class LoginController {
             httpSession.setAttribute("error", e.getMessage());
             return "redirect:/";
         }
+        httpSession.removeAttribute("error");
         return "redirect:/welcome";
     }
 
 
+    /*validateUser is checking whether the user is logged in by first checking if the HttpSession attribute "email" & "password" has been set.
+    * Afterwards it will check if they match the email and password in the database. */
     //Frederik
     public boolean validateUser(HttpSession httpSession) throws CarLeasingException {
 
@@ -78,9 +100,11 @@ public class LoginController {
             User user = userService.getEmailAndPassword((String) httpSession.getAttribute("email"), (String) httpSession.getAttribute("password"));
             return user.getEmail().equals(httpSession.getAttribute("email")) && user.getPassword().equals((httpSession.getAttribute("password")));
         }
-        throw new CarLeasingException(exceptionEnums.get(carExceptionEnum.NO_LOGIN));
+        throw new CarLeasingException(exceptionEnums.get(carExceptionEnum.NO_LOGIN)) ;
     }
 
+    /* validateRoles will first find the user based on the httpsession attributes. As these were already validated in the previous method,
+    we therefore don't have to validate them again. A rolelist will then be created for the user */
     //Frederik
     public List<String> validateRoles(HttpSession httpSession) throws CarLeasingException {
         User user = userService.getEmailAndPassword((String) httpSession.getAttribute("email"), (String) httpSession.getAttribute("password"));
@@ -88,6 +112,9 @@ public class LoginController {
         return roleList;
     }
 
+    /*welcomeUser is firstly validating the login. Afterwards modelattributes will be set to display an overview of all functions that the user has access to.
+    * This method also catches exceptions fx. a user tries to visit a page they do not have access to. Then they will be returned to the welcome page with an error message.
+    * */
     //Frederik
     @GetMapping("/welcome")
     public String welcomeUser(HttpSession httpSession, Model model) throws CarLeasingException {
@@ -99,18 +126,18 @@ public class LoginController {
             httpSession.setAttribute("error", e.getMessage());
             return "redirect:/";
         }
-        if (httpSession.getAttribute("error") != null && !httpSession.getAttribute("error").equals("") &&
-                (!httpSession.getAttribute("error").equals(exceptionEnums.get(carExceptionEnum.WRONG_LOGIN))) &&
-                (!httpSession.getAttribute("error").equals(exceptionEnums.get(carExceptionEnum.NO_LOGIN)))) {
+        if (httpSession.getAttribute("error") != null) {
             model.addAttribute("errorMessage", httpSession.getAttribute("error"));
         }
-        httpSession.setAttribute("error","");
+        httpSession.removeAttribute("error");
         model.addAttribute("roles", validateRoles(httpSession));
-        httpSession.setAttribute("roller", validateRoles(httpSession));
+//        httpSession.setAttribute("roller", validateRoles(httpSession));
         model.addAttribute("name", userService.getEmailAndPassword((String) httpSession.getAttribute("email"), (String) httpSession.getAttribute("password")).getFirstName());
         return "welcome";
     }
 
+    /*Lastly the LoginController has a page called invalidate-cookie, which will invalidate the session, meaning that all objects assigned to it will be removed.
+    This functions as a logout button since when pressed, the user is returned back to the login page due the session now being invalid*/
     //Frederik
     @GetMapping("/invalidate-cookie")
     public String logOut(HttpSession session) {
