7149c769fd89df67674ab1ae02a934e1714a2c4f
Linding3r <thomasjuullindinger@yahoo.dk>
2022-12-12 08:50:45 +0100

    UsernameMakerTest

--- src/test/java/com/example/bilabonnement/security/UsernameMakerTest.java
+++ src/test/java/com/example/bilabonnement/security/UsernameMakerTest.java
@@ -1,15 +1,36 @@
 package com.example.bilabonnement.security;
 
+import com.example.bilabonnement.repository.UserRepository;
+import com.example.bilabonnement.service.UserService;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Date;
+import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class UsernameMakerTest {
 
+    UserRepository userRepository;
+    UserService userService;
+    UsernameMaker usernameMaker;
+
     @Test
     void makeUsername() {
+
+        //arrange
+        userRepository = new UserRepository();
+
+        userService = new UserService(userRepository);
+
+        usernameMaker = new UsernameMaker(userService);
+
+
+        //act
+        java.sql.Date date = new java.sql.Date(1995, 12, 12);
+        String username = usernameMaker.makeUsername("Frederik", "Hansen", date);
+
+        //assert
+        assert username.equals("f_hansen1995");
+
 
     }
 }
