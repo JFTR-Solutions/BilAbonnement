201c8e3dd43678bc87b23703be14272c07d994d1
FHallengreen <fred70p9@stud.kea.dk>
2022-12-15 11:05:22 +0100

    Added comments to exception classes

--- src/main/java/com/example/bilabonnement/exceptions/CarLeasingException.java
+++ src/main/java/com/example/bilabonnement/exceptions/CarLeasingException.java
@@ -3,6 +3,15 @@ package com.example.bilabonnement.exceptions;
 import java.util.HashMap;
 
 //FREDERIK
+
+/*This code defines a custom CarLeasingException class that extends the RuntimeException class.
+A RuntimeException is a type of exception that occurs during the execution of a program, and is not checked by the compiler.
+
+The CarLeasingException class has a constructor that takes a message string as an argument.
+This message is passed to the super class (RuntimeException) and will be used to provide information about the exception when it is thrown.
+The CarLeasingException class can be thrown in a program to indicate that an error has occurred related to car leasing. This allows the application to provide a custom response to the exception.
+*/
+
 public class CarLeasingException extends RuntimeException {
 
     public CarLeasingException(String message) {
--- src/main/java/com/example/bilabonnement/exceptions/CarLeasingExceptionHandler.java
+++ src/main/java/com/example/bilabonnement/exceptions/CarLeasingExceptionHandler.java
@@ -9,6 +9,13 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 //Frederik
+
+/*This class is used to handle exceptions thrown by the controller in our spring application
+* The handleCarLeasingException method is annotated with @ExceptionHandler, indicating that it will be called to handle
+* any CarLeasingExceptions thrown in the application.
+* The method creates a CarException object containing details about the exception, including the exception message and the HTTP status code,
+* and returns it as a response entity with the appropriate HTTP status code. This allows the application to provide a customized response to any CarLeasingExceptions that are thrown.
+* */
 @ControllerAdvice
 public class CarLeasingExceptionHandler {
 
